55867bafaaa3b1a011984f3447a91fb887ec0644
Claudia <calucita@users.noreply.github.com>
2019-07-21 09:38:52 +0000

    now secure! (ish)
    adding keyring functionality :)

--- Settings.py
+++ Settings.py
@@ -1,27 +1,48 @@
 import os
+import keyring
 fileName=os.getcwd()+"\\credentials.txt"
+service="Whostalking"
 
 def loadCredentials(app):
-    try:
-        creds = open(fileName, 'r')
-        for line in creds:
-            if "PASS" in line:
-                loadMsg(app.OauthEntry, line)
-            elif "IDENT" in line:
-                loadMsg(app.NameEntry, line)
-            elif "CHANNEL" in line:
-                loadMsg(app.ChannelEntry, line)
-        creds.close()
-    except:
-        pass
+    # if this file exists, let's fix it
+    if os.path.isfile(fileName):
+        try:
+            
+            creds = open(fileName, 'r')
+            for line in creds:
+                if "PASS" in line:
+                    loadMsg(app.OauthEntry, line)
+                elif "IDENT" in line:
+                    loadMsg(app.NameEntry, line)
+                elif "CHANNEL" in line:
+                    loadMsg(app.ChannelEntry, line)
+            creds.close()
+            os.remove(fileName)
+            saveCredentials(app)
+        except:
+            pass
+    else:
+        botto = keyring.get_password(service, service)
+        if not botto:
+            return
+
+        app.NameEntry.insert(0, str(botto))
+        channel = keyring.get_password(service, botto)
+        if not channel:
+            return
+        
+        app.ChannelEntry.insert(0, str(channel))
+        oauth = keyring.get_password(service, botto+channel)
+        if not oauth:
+            return
+        
+        app.OauthEntry.insert(0, oauth)
+    
     
 def saveCredentials(app):
-    try:
-        creds = open(fileName, 'w')
-        creds.write("PASS="+str(app.OauthEntry.get())+"\n" + "IDENT="+str(app.NameEntry.get())+"\n" + "CHANNEL="+str(app.ChannelEntry.get())+"\n")
-        creds.close()
-    except:
-        pass
+    keyring.set_password(service, service, app.NameEntry.get())
+    keyring.set_password(service, app.NameEntry.get(), app.ChannelEntry.get())
+    keyring.set_password(service, app.NameEntry.get()+app.ChannelEntry.get(), app.OauthEntry.get())
     
 def loadMsg(entry, line):
     entry.delete(0, 'end')
