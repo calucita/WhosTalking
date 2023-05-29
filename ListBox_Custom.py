d24cd8699d69a7e79533da198097a7780869a5db
Claudia <calucita@users.noreply.github.com>
2023-05-29 21:28:37 +0200

    alternating shades for the list

--- ListBox_Custom.py
+++ ListBox_Custom.py
@@ -6,11 +6,18 @@ class ListBox_Custom(customtkinter.CTkScrollableFrame):
         super().__init__(master, **kwargs)
         self.grid_columnconfigure(0, weight=1)
         self._list = []
+        self._lastColor = False
 
     def add_item(self, text: str) -> None:
-        label = customtkinter.CTkLabel(self, text=text, compound="left", padx=5, anchor="w")
+        color = "transparent"
+        if not self._lastColor:
+            color = ("gray90", "gray25")
+        self._lastColor = not self._lastColor
+        label = customtkinter.CTkLabel(
+            self, text=text, compound="left", padx=5, anchor="w", fg_color=color, corner_radius=3
+        )
 
-        label.grid(row=len(self._list), column=0, pady=(0, 10), sticky="w")
+        label.grid(row=len(self._list), column=0, sticky="w" + "e")
         self._list.append(label)
 
     def remove_item(self, text: str) -> None:
